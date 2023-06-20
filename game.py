f85d7ccd7f29542d6b2e317b8bbfa12d984959f7
Jay Lin <jenjenuwu@MacBook-Pro-2019.local>
2023-06-20 14:17:32 -0400

    completion of summitive

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 import chess
 import pygame
+import time
 from pygame.locals import *
-from pieces import CustomPiece
-from chess.variant import *
-from chess import *
+from chess.variant import *  # Importing chess variant-related modules
+from chess import *  # Importing the chess module
+
 # Constants for the graphical representation
 WINDOW_WIDTH = 512
 WINDOW_HEIGHT = 512
 BOARD_SIZE = 8
 SQUARE_SIZE = WINDOW_WIDTH // BOARD_SIZE
 
-# attack constants
+# Attack functions
+
+# Function to calculate sliding attacks for a given square, occupied squares, and deltas
 
 
 def custom_sliding_attacks(square: Square, occupied: Bitboard, deltas: Iterable[int]) -> Bitboard:
@@ -32,9 +35,14 @@ def custom_sliding_attacks(square: Square, occupied: Bitboard, deltas: Iterable[
 
     return attacks
 
+# Function to calculate step attacks for a given square and deltas
+
 
 def custom_step_attacks(square: Square, deltas: Iterable[int]) -> Bitboard:
     return custom_sliding_attacks(square, BB_ALL, deltas)
+
+# Function to calculate sliding attacks with a maximum distance for a given square, occupied squares, deltas, and maximum distance
+
 
 def _sliding_attacks(square: Square, occupied: Bitboard, deltas: Iterable[int], max_distance: int) -> Bitboard:
     attacks = BB_EMPTY
@@ -58,6 +66,7 @@ def _sliding_attacks(square: Square, occupied: Bitboard, deltas: Iterable[int], 
 
     return attacks
 
+# Function to generate an attack table for a given set of deltas
 
 
 def custom_attack_table(deltas: List[int]) -> Tuple[List[Bitboard], List[Dict[Bitboard, Bitboard]]]:
@@ -67,37 +76,50 @@ def custom_attack_table(deltas: List[int]) -> Tuple[List[Bitboard], List[Dict[Bi
     for square in SQUARES:
         attacks = {}
 
-        mask = _sliding_attacks(square, 0, deltas,4) & ~chess._edges(square)
+        mask = _sliding_attacks(square, 0, deltas, 4) & ~chess._edges(square)
         for subset in chess._carry_rippler(mask):
-            attacks[subset] = _sliding_attacks(square, subset, deltas,4)
+            attacks[subset] = _sliding_attacks(square, subset, deltas, 4)
 
         attack_table.append(attacks)
         mask_table.append(mask)
 
     return mask_table, attack_table
+# Attack constants
 
 
-
+# Pre-computed knight attacks for each square
 BB_KNIGHT_ATTACKS = [custom_step_attacks(
     sq, [25, 23, 11, 5, -25, -23, -11, -5]) for sq in SQUARES]
+
+# Pre-computed king attacks for each square
 BB_KING_ATTACKS = [chess._step_attacks(sq, [8, 1, -8, -1]) for sq in SQUARES]
+
+# Pre-computed pawn attacks for each square and color
 BB_PAWN_ATTACKS = [[chess._step_attacks(
     sq, deltas) for sq in SQUARES] for deltas in [[-8], [8]]]
+
+# Pre-computed diagonal mask and attack tables using custom deltas
 BB_DIAG_MASKS, BB_DIAG_ATTACKS = custom_attack_table([-9, -7, 7, 9])
+
+# Pre-computed file mask and attack tables using custom deltas
 BB_FILE_MASKS, BB_FILE_ATTACKS = custom_attack_table([-8, 8])
+
+# Pre-computed rank mask and attack tables using custom deltas
 BB_RANK_MASKS, BB_RANK_ATTACKS = custom_attack_table([-1, 1])
 
-
 # Colors
-LIGHT_SQUARE_COLOR = (240, 217, 181)
-DARK_SQUARE_COLOR = (181, 136, 99)
+# Color for light squares on the chessboard
+LIGHT_SQUARE_COLOR = (194, 209, 165)
+DARK_SQUARE_COLOR = (110, 128, 93)  # Color for dark squares on the chessboard
+# Color for selected square on the chessboard
 SELECTED_SQUARE_COLOR = (122, 158, 196)
-MOVE_SQUARE_COLOR = (100,100,100)
+# Color for valid move squares on the chessboard
+MOVE_SQUARE_COLOR = (100, 100, 100)
 
 # Images for the chess pieces
 PIECE_IMAGES = {
     chess.PAWN: {
-        chess.WHITE: pygame.image.load("images/bbb.png"),
+        chess.WHITE: pygame.image.load("images/wp.png"),
         chess.BLACK: pygame.image.load("images/bp.png"),
     },
     chess.ROOK: {
@@ -137,9 +159,11 @@ class CustomBoard(chess.variant.RacingKingsBoard):
         super().__init__(fen, chess960=chess960)
 
     def reset(self) -> None:
+        # Resets the board to the starting position
         self.set_fen(type(self).starting_fen)
 
     def is_variant_end(self) -> bool:
+        # Checks if the variant end condition is met
         RANK8 = (chess.A8, chess.B8, chess.C8, chess.D8,
                  chess.E8, chess.F8, chess.G8, chess.H8)
         RANK1 = (chess.A1, chess.B1, chess.C1, chess.D1,
@@ -150,25 +174,32 @@ class CustomBoard(chess.variant.RacingKingsBoard):
             return True
 
     def attacks_mask(self, square: Square) -> Bitboard:
+        # Returns the attack bitboard for a given square
         bb_square = BB_SQUARES[square]
 
         if bb_square & self.pawns:
+            # If the square is occupied by a pawn
             color = bool(bb_square & self.occupied_co[WHITE])
             return BB_PAWN_ATTACKS[color][square]
         elif bb_square & self.knights:
+            # If the square is occupied by a knight
             return BB_KNIGHT_ATTACKS[square]
         elif bb_square & self.kings:
+            # If the square is occupied by a king
             return BB_KING_ATTACKS[square]
         else:
+            # If the square is occupied by a bishop or queen
             attacks = 0
             if bb_square & self.bishops or bb_square & self.queens:
-                attacks = BB_DIAG_ATTACKS[square][BB_DIAG_MASKS[square] & self.occupied]
+                attacks = BB_DIAG_ATTACKS[square][BB_DIAG_MASKS[square]
+                                                  & self.occupied]
             if bb_square & self.rooks or bb_square & self.queens:
                 attacks |= (BB_RANK_ATTACKS[square][BB_RANK_MASKS[square] & self.occupied] |
                             BB_FILE_ATTACKS[square][BB_FILE_MASKS[square] & self.occupied])
             return attacks
 
     def _attackers_mask(self, color: Color, square: Square, occupied: Bitboard) -> Bitboard:
+        # Returns the attackers bitboard for a given color, square, and occupied squares
         rank_pieces = BB_RANK_MASKS[square] & occupied
         file_pieces = BB_FILE_MASKS[square] & occupied
         diag_pieces = BB_DIAG_MASKS[square] & occupied
@@ -185,8 +216,9 @@ class CustomBoard(chess.variant.RacingKingsBoard):
             (BB_PAWN_ATTACKS[not color][square] & self.pawns))
 
         return attackers & self.occupied_co[color]
-    
+
     def generate_pseudo_legal_moves(self, from_mask: Bitboard = BB_ALL, to_mask: Bitboard = BB_ALL) -> Iterator[Move]:
+        # Generates pseudo-legal moves from a given from_mask and to_mask
         our_pieces = self.occupied_co[self.turn]
 
         # Generate piece moves.
@@ -224,10 +256,12 @@ class CustomBoard(chess.variant.RacingKingsBoard):
         # Prepare pawn advance generation.
         if self.turn == WHITE:
             single_moves = pawns << 8 & ~self.occupied
-            double_moves = single_moves << 8 & ~self.occupied & (BB_RANK_3 | BB_RANK_4)
+            double_moves = single_moves << 8 & ~self.occupied & (
+                BB_RANK_3 | BB_RANK_4)
         else:
             single_moves = pawns >> 8 & ~self.occupied
-            double_moves = single_moves >> 8 & ~self.occupied & (BB_RANK_6 | BB_RANK_5)
+            double_moves = single_moves >> 8 & ~self.occupied & (
+                BB_RANK_6 | BB_RANK_5)
 
         single_moves &= to_mask
         double_moves &= to_mask
@@ -253,8 +287,9 @@ class CustomBoard(chess.variant.RacingKingsBoard):
         if self.ep_square:
             yield from self.generate_pseudo_legal_ep(from_mask, to_mask)
 
-# Usage
+
 def draw_board(screen):
+    # Draws the chessboard on the screen
     for row in range(BOARD_SIZE):
         for col in range(BOARD_SIZE):
             if (row + col) % 2 == 0:
@@ -266,6 +301,7 @@ def draw_board(screen):
 
 
 def draw_pieces(screen, board):
+    # Draws the chess pieces on the screen based on the current board state
     for row in range(BOARD_SIZE):
         for col in range(BOARD_SIZE):
             square = chess.square(col, BOARD_SIZE - row - 1)
@@ -277,26 +313,36 @@ def draw_pieces(screen, board):
 
 
 def get_square(pos):
+    # Converts a screen position to a chess square
     row = pos[1] // SQUARE_SIZE
     col = pos[0] // SQUARE_SIZE
     return chess.square(col, BOARD_SIZE - row - 1)
 
 
 def main():
+    # Initialize pygame and create the game window
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Chess Game")
 
+    # Create a clock object to control the frame rate
     clock = pygame.time.Clock()
+
+    # Create the chessboard object
     board = CustomBoard()
+
+    # Initialize variables for handling user input and game state
     selected_square = None
     gameover = False
+    winner_text = None  # Text to display the winner
+    end_time = None  # End time for displaying the winner
+
+    # Main game loop
     while not gameover:
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             if event.type == MOUSEBUTTONDOWN:
-
                 if event.button == 1:
                     pos = pygame.mouse.get_pos()
                     square = get_square(pos)
@@ -315,17 +361,21 @@ def main():
         screen.fill((255, 255, 255))
         draw_board(screen)
         draw_pieces(screen, board)
+
         if selected_square is not None:
-            transparent_color = (*SELECTED_SQUARE_COLOR, 150)  # Add alpha value to the color tuple
+            # Add alpha value to the color tuple
+            transparent_color = (*SELECTED_SQUARE_COLOR, 150)
 
             # Create a surface with per-pixel alpha
-            rect_surface = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE), pygame.SRCALPHA)
-            pygame.draw.rect(rect_surface, transparent_color, rect_surface.get_rect())
+            rect_surface = pygame.Surface(
+                (SQUARE_SIZE, SQUARE_SIZE), pygame.SRCALPHA)
+            pygame.draw.rect(rect_surface, transparent_color,
+                             rect_surface.get_rect())
 
             # Blit the rectangle surface onto the main surface
             screen.blit(rect_surface, (chess.square_file(selected_square) * SQUARE_SIZE,
-                                    (BOARD_SIZE - chess.square_rank(selected_square) - 1) * SQUARE_SIZE))
-            
+                                       (BOARD_SIZE - chess.square_rank(selected_square) - 1) * SQUARE_SIZE))
+
             possible_moves = []
             for move in board.legal_moves:
                 if move.from_square == selected_square:
@@ -335,22 +385,42 @@ def main():
 
             for move in possible_moves:
                 move_square = move[1]
-                alpha_surface = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE), pygame.SRCALPHA)
+                alpha_surface = pygame.Surface(
+                    (SQUARE_SIZE, SQUARE_SIZE), pygame.SRCALPHA)
                 circle_color = (*MOVE_SQUARE_COLOR, 200)
                 circle_center = (SQUARE_SIZE // 2, SQUARE_SIZE // 2)
                 circle_radius = SQUARE_SIZE // 8
-                pygame.draw.circle(alpha_surface, circle_color, circle_center, circle_radius)
+                pygame.draw.circle(alpha_surface, circle_color,
+                                   circle_center, circle_radius)
                 screen.blit(alpha_surface, (chess.square_file(move_square) * SQUARE_SIZE,
                                             (7 - chess.square_rank(move_square)) * SQUARE_SIZE))
-
 
         pygame.display.flip()
         clock.tick(60)
 
+        # Check if the game has ended
         if board.is_variant_end():
+            result = board.result()
+            if result == "1-0":
+                winner_text = "White wins!"
+            elif result == "0-1":
+                winner_text = "Black wins!"
+            else:
+                winner_text = "It's a draw!"
+            end_time = time.time() + 5  # Display winner for 5 seconds
+
+        # Display the winner text and wait for 5 seconds
+        if winner_text:
+            font = pygame.font.Font(None, 36)
+            text = font.render(winner_text, True, (0, 0, 0))
+            text_rect = text.get_rect(
+                center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
+            screen.blit(text, text_rect)
+            pygame.display.flip()
+            time.sleep(5)
+
+        # Check if the time for displaying the winner has elapsed
+        if end_time and time.time() >= end_time:
             gameover = True
+
     return
-
-
-if __name__ == "__main__":
-    main()
--- menu.py
+++ menu.py
@@ -1,6 +1,8 @@
+# Import necessary libraries
 import pygame
 import sys
 from game import main
+
 # Initialize Pygame
 pygame.init()
 
@@ -14,20 +16,27 @@ BLACK = (0, 0, 0)
 
 # Create the window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Menu Example")
+pygame.display.set_caption("Game Menu")
+
+# Load and scale the menu background image
+background_image = pygame.image.load("images/background.jpg")
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
 # Define button dimensions
 BUTTON_WIDTH = 200
 BUTTON_HEIGHT = 50
 BUTTON_MARGIN = 20
 
-# Create font
-font = pygame.font.Font(None, 40)
+# Create font for buttons
+button_font = pygame.font.Font(None, 40)
+
+# Create font for the title
+title_font = pygame.font.Font("images/pixel_font.ttf", 72)  # Replace "title_font.ttf" with your chosen font file
 
 # Create "Start Game" button
 start_button = pygame.Rect(
     WIDTH // 2 - BUTTON_WIDTH // 2,
-    HEIGHT // 2 - BUTTON_HEIGHT - BUTTON_MARGIN // 2,
+    HEIGHT // 2 + BUTTON_MARGIN,
     BUTTON_WIDTH,
     BUTTON_HEIGHT,
 )
@@ -35,7 +44,7 @@ start_button = pygame.Rect(
 # Create "Quit" button
 quit_button = pygame.Rect(
     WIDTH // 2 - BUTTON_WIDTH // 2,
-    HEIGHT // 2 + BUTTON_MARGIN // 2,
+    HEIGHT // 2 + BUTTON_HEIGHT + BUTTON_MARGIN * 2,
     BUTTON_WIDTH,
     BUTTON_HEIGHT,
 )
@@ -48,6 +57,7 @@ in_game = False  # Flag to indicate if the game is in progress
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            # Quit the game if the window is closed
             running = False
             pygame.quit()
             sys.exit()
@@ -57,6 +67,7 @@ while running:
 
             # Check if "Start Game" button is clicked
             if start_button.collidepoint(mouse_pos):
+                # Start the game if the button is clicked
                 print("Start Game clicked")
                 in_game = True
                 main()
@@ -64,21 +75,22 @@ while running:
 
             # Check if "Quit" button is clicked
             if quit_button.collidepoint(mouse_pos):
+                # Quit the game if the button is clicked
                 print("Quit clicked")
                 running = False
                 pygame.quit()
                 sys.exit()
 
-    # Clear the screen
-    screen.fill(WHITE)
+    # Blit the background image onto the screen
+    screen.blit(background_image, (0, 0))
 
     # Draw buttons
     pygame.draw.rect(screen, BLACK, start_button)
     pygame.draw.rect(screen, BLACK, quit_button)
 
-    # Render text
-    start_text = font.render("Start Game", True, WHITE)
-    quit_text = font.render("Quit", True, WHITE)
+    # Render text for buttons
+    start_text = button_font.render("Start Game", True, WHITE)
+    quit_text = button_font.render("Quit", True, WHITE)
 
     # Center the text on buttons
     start_text_rect = start_text.get_rect(center=start_button.center)
@@ -87,6 +99,17 @@ while running:
     # Draw text on buttons
     screen.blit(start_text, start_text_rect)
     screen.blit(quit_text, quit_text_rect)
+
+    # Render and position the title text
+    title_lines = ["Survive", "The", "Savana"]
+    title_texts = [title_font.render(line, True, BLACK) for line in title_lines]
+    title_height = sum([text.get_height() for text in title_texts])
+    title_y = (HEIGHT - title_height - BUTTON_HEIGHT * 2 - BUTTON_MARGIN * 4) // 2
+
+    for i, text in enumerate(title_texts):
+        text_rect = text.get_rect(center=(WIDTH // 2, title_y + text.get_height() // 2))
+        screen.blit(text, text_rect)
+        title_y += text.get_height()
 
     # Update the display
     pygame.display.update()
